3943a029af50301c9d4242dc05d944ba0b2cfa42
Eirik Vikanes <eirik.vikanes@gmail.com>
2018-02-20 19:33:31 +0100

    Moved stateful LSTM to separate file, added new data files

--- models/lstm-stateful/main.py
+++ models/lstm-stateful/main.py
@@ -35,6 +35,10 @@ class RNN:
         data_x = self.dataset.iloc[:, :-1]
         data_y = self.dataset.iloc[:, -1:]
 
+        # print(data_x)
+        # print(data_y)
+        # exit(0)
+
         #Normalizing data
         self.x_scaler = MinMaxScaler(copy=True, feature_range=(0,1))
         data_x = self.x_scaler.fit_transform(data_x)
@@ -60,11 +64,13 @@ class RNN:
         self.timeseries_data = self.timeseries_np.reshape(self.timeseries_np.shape[0], n_in+n_out ,self.data.shape[1])
 
         # Data is everything but the two last rows in the third dimension (which contain the delayed and actual production values)
-        self.x_data = self.timeseries_data[:, :, :-1]
-        self.y_data = self.timeseries_data[:, :, -1:]
+        self.x_data = self.timeseries_data[:self.timeseries_data.shape[0]-self.timeseries_data.shape[0] % self.batch_size, :,:-2]
+        self.y_data = self.timeseries_data[:self.timeseries_data.shape[0]-self.timeseries_data.shape[0] % self.batch_size,:,-1:]
 
         #Split for dividing the dataset in a factor of the batch size
-        split = int(self.testsplit * self.x_data.shape[0])
+        split = self.testsplit * self.x_data.shape[0]
+        split -= split % self.batch_size
+        split = int(split)
 
         # Create training and test sets for x
         self.x_train = self.x_data[:split, :, :]
@@ -73,11 +79,24 @@ class RNN:
         # Create training and test sets for y
         self.y_train = self.y_data[:split, 0, :]
         self.y_test = self.y_data[split:, 0, :]
+        # self.y_train = self.y_data[:split, :, :]
+        # self.y_test = self.y_data[split:, :, :]
+
+        assert self.x_train.shape[0] % self.batch_size == 0, 'training sample size not divisible by batch size'
+        assert self.x_test.shape[0] % self.batch_size == 0, 'testing sample size not divisible by batch size'
 
     def build_model(self):
         self.model = Sequential()
 
-        self.model.add(LSTM(32, return_sequences=True, input_shape=(self.x_train.shape[1], self.x_train.shape[2])))
+        self.model.add(LSTM(32, return_sequences=True,
+                                #activation='softsign',
+                                batch_input_shape=(self.batch_size, self.x_train.shape[1], self.x_train.shape[2]),
+                                stateful=True))
+        ))
+
+        # self.model.add(Dropout(0.15))
+        
+        #self.model.add(LSTM(64, return_sequences=True, activation='softsign'))
 
         self.model.add(LSTM(16))
         
@@ -87,13 +106,39 @@ class RNN:
 
     def train_network(self):
         self.model.compile(loss='mae', optimizer='adam', metrics=['mae','mse',self.rmse])
-        self.model.fit(x=self.x_train, y=self.y_train, batch_size=self.batch_size, validation_split=0.2, epochs = self.batch_size, verbose=1, shuffle=False)
+
+        # Self-written early_stopping
+        patience = 10
+        epochs_since_best = 0
+        best_val_loss = float('inf')
+
+        for i in range(self.epochs):
+            log = self.model.fit(x=self.x_train, y=self.y_train, batch_size=self.batch_size, validation_split=0.2, epochs = 1, verbose=1, shuffle=False)
+            
+            # Early stopping
+            epochs_since_best += 1
+            if log.history['val_loss'][0] < best_val_loss:
+                self.model.save(os.path.join('checkpoint_model.h5'))
+                print("Improved model, saving...")
+                epochs_since_best = 0
+                best_val_loss = log.history['val_loss']
+            elif epochs_since_best >= patience:
+                print("Exceeded patience, halting training...")
+                break
+                
+            print()
+            
+            #Resetting states
+            #self.model.reset_states()
+            print('Epoch: %.d' % (i + 1))
 
     def predict(self):
         # Load best found model
         self.model.load_weights(os.path.join('checkpoint_model.h5'))
 
         self.predictions = self.model.predict(self.x_test, batch_size=self.batch_size)
+        # self.predictions_last = self.predictions[:,-1,:]
+        # self.y_test_last = self.y_test[:,-1,:]
 
         self.evaluation = self.model.evaluate(self.x_test, self.y_test, batch_size=self.batch_size)
 
@@ -101,6 +146,10 @@ class RNN:
         print(self.model.metrics_names)
         print(self.evaluation)
         print()
+
+        # print('MAE')
+        # print(mean_absolute_error(self.y_test, self.predictions))
+        #print(mean_absolute_error(self.y_test_last, self.predictions_last))
 
     #RMSE loss function (missing in keras library)
     def rmse_numpy(self, y_true, y_pred):
@@ -134,15 +183,22 @@ class RNN:
         return agg
 
     def visualize(self):
-        lines = pyplot.plot(self.predictions, 'r', self.y_test, 'b')
+        inverse_predictions = self.predictions
+        inverse_actuals = self.y_test
+        # inverse_predictions = self.predictions_last
+        # inverse_actuals = self.y_test_last
+
+        lines = pyplot.plot(inverse_predictions, 'r', inverse_actuals, 'b')
         pyplot.setp(lines, linewidth=0.5)
         pyplot.show()
 
 if __name__ == '__main__':
-    datapath = os.path.join('data', 'Data_advanced.csv')
+    datapath = os.path.join('data', 'data-3.0.csv')
 
     nn_network = RNN(datapath)
     nn_network.build_model()
     nn_network.train_network()
     nn_network.predict()
     nn_network.visualize()
+    
+    exit(0)
