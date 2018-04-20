4386c79fb8ec30386e39b30562a4d5a90e155c8f
fabiansd <fabiansd1402@gmail.com>
2018-04-20 09:46:57 +0200

    merging med master

--- models/nn_dual_loss/nn_dual_loss.py
+++ models/nn_dual_loss/nn_dual_loss.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split, KFold
 from keras import backend as K
-from keras.layers import Input, Dense, LeakyReLU, BatchNormalization, Average, Dropout, Concatenate, GaussianNoise
+from keras.layers import Input, Dense, LeakyReLU, BatchNormalization, Average, Dropout, Concatenate
 from keras.models import Model, Sequential
 from keras import optimizers
 from keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -24,72 +24,35 @@ def gen_batch(X1, X2, batch_size):
         yield x1, x2
 
 
-class NN_dual:
+class NN_feedback:
 
-    def __init__(self, model_path, batch_size=32, epochs=1000, dropoutrate=0.3):
+    def __init__(self, batch_size=32, epochs=1000, dropoutrate=0.3):
         self.batch_size = batch_size
         self.epochs = epochs
         self.dropout_rate = dropoutrate
         self.relu_leak = 0.2
-        self.model_path = model_path
 
-    # Builds several dense neural networks, trains them and
-    def build_forest_model(self, input_dim, model_structure):
-        input_layer = Input(shape=(input_dim,))
-        network_list = []
-
-        for i, network in enumerate(model_structure):
-            network_list.append(self.dense_block(
-                input_data=input_layer, units=network[0][0], dropout=network[0][1]))
-            for layer in network[1:]:
-
-                if layer[0] != 0:
-                    network_list[i] = self.dense_block(
-                        input_data=network_list[i], units=layer[0], dropout=layer[1])
-
-            network_list[i] = Dense(1)(network_list[i])
-
-        out_avg = Average()(network_list)
-
-        self.model = Model(inputs=input_layer, outputs=out_avg)
-        return self.model
-
-    def build_model(self, input_dim, output_dim):
+    # Build n networks and averages the final dense output
+    def build_feedback_model(self, input_dim, num_windmills, model_structure):
         input_layer = Input(shape=(input_dim,))
 
-        # x1 = self.dense_block(input_layer, 128, False, 0)
-        x1 = self.dense_block(input_layer, 64, True, 0)
+        x1 = self.dense_block(input_layer, 64, False, 0)
         x2 = self.dense_block(x1, 32, False, 0)
         x3 = self.dense_block(x2, 16, False, 0)
         x4 = self.dense_block(x3, 8, False, 0)
         x5 = self.dense_block(x4, 2, False, 0)
 
-        # single_prod = Dense(num_windmills)(x3)
+        single_prod = Dense(num_windmills)(x3)
 
-        total = Dense(1)(x4)
+        # for layer in model_structure[1:]:
 
-        # , single_prod])
-        self.model = Model(inputs=input_layer, outputs=total)
-        self.model.summary()
-        return self.model
+        #     if layer[0] != 0:
+        #         x = self.dense_block(input_data = x, units = layer[0], dropout = layer[1])
 
-    def build_model_single_targets(self, input_dim, output_dim):
-        input_layer = Input(shape=(input_dim,))
-        # x0 = GaussianNoise(0.15)(input_layer)
-        # x1 = self.dense_block(input_layer, 128, False, 0)
-        x1 = self.dense_block(input_layer, 100, False, 0)
-        x2 = self.dense_block(x1, 60, True, 0)
-        x3 = self.dense_block(x2, 40, False, 0)
-        # x4 = self.dense_block(x3, 30, False, 0)
+        total = Dense(1)(x5)
 
-        # single_prod = Dense(num_windmills)(x3)
-
-        total = Dense(output_dim)(x3)
-
-        # , single_prod])
-        self.model = Model(inputs=input_layer, outputs=total)
-        self.model.summary()
-        return self.model
+        self.model = Model(inputs=input_layer, outputs=[total, single_prod])
+        # self.model.summary()
 
     def dense_block(self, input_data, units, dropout=False, l2_reg=0):
 
@@ -100,51 +63,26 @@ class NN_dual:
         x = BatchNormalization()(x)
         return LeakyReLU(self.relu_leak)(x)
 
-    def train_network(self, x_train, y_train, opt='adam', validation_split=0.2):
-        self.model.compile(loss='mae', optimizer=opt,
-                           metrics=['mae'])
+    def train_network(self, x_train, y_train, y_train_vector, opt='adam', validation_split=0.15):
+        self.model.compile(loss=['mae', 'binary_crossentropy'], optimizer=opt,
+                           metrics=['mae', 'mse', self.rmse])
 
-        early_stopping = EarlyStopping(monitor='val_loss', patience=600)
+        early_stopping = EarlyStopping(monitor='val_loss', patience=500)
         checkpoint = ModelCheckpoint(
             'checkpoint_model.h5', monitor='val_loss', verbose=0, save_best_only=True, mode='min')
 
         # Train the model
-        history = self.model.fit(x=x_train, y=y_train, batch_size=self.batch_size, validation_split=validation_split, callbacks=[
-            early_stopping, checkpoint], epochs=self.epochs, verbose=2, shuffle=True)
 
-        return history.history, self.model
+        self.model.fit(x=x_train, y=[y_train, y_train_vector], batch_size=self.batch_size, validation_split=validation_split, callbacks=[
+                       early_stopping, checkpoint], epochs=self.epochs, verbose=2, shuffle=True)
 
-    def predict(self, x):
+    def evaluate(self, model_path, x_test, y_test):
         self.model.compile(loss='mae', optimizer='adam',
-                           metrics=['mae'])
-        self.model.load_weights(self.model_path)
+                           metrics=['mae', 'mse', self.rmse])
+        self.model.load_weights(model_path)
 
-        return self.model.predict(x)
-
-    def evaluate(self, x_test, y_test, single_targets):
-        self.model.compile(loss='mae', optimizer='adam',
-                           metrics=['mae'])
-        self.model.load_weights(self.model_path)
-
-        if not single_targets:
-            evaluation = self.model.evaluate(x_test, y_test)
-
-        else:
-            predictions = self.model.predict(x_test)
-            print(predictions.shape)
-            predictions = np.sum(predictions, axis=1)
-            gt = np.sum(y_test, axis=1)
-            print('eval')
-            evaluation = np.mean(np.abs(predictions - gt))
-
-        print(evaluation)
-
+        evaluation = self.model.evaluate(x_test, y_test)
         return evaluation, self.model.metrics_names
-
-    def correlation_analysis(x, y):
-        pred = self.model.predict(x)
-
-        indices = np.argsort(feature_analysis)[::-1]
 
     def visualize_model(self):
         from keras.utils import plot_model
--- util/data_analysis.py
+++ util/data_analysis.py
@@ -14,68 +14,8 @@ from sklearn.metrics import mean_absolute_error
 
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
 
-
-def feature_importance(dataset, scope=None, num_features=20, print_=True, n_jobs=4):
-
-    dataset = dataset.dropna()
-
-    if scope == None or scope >= dataset.shape[0]:
-        scope = dataset.shape[0]
-
-    x = dataset.iloc[:scope, :-1].values
-    y = dataset.iloc[:scope, -1:].values.ravel()
-
-    features = dataset.columns.values[:-1]
-
-    print('\nFitting random forest with {} samples\n'.format(scope))
-
-    regressor = RandomForestRegressor(bootstrap=True, criterion='mae', max_depth=20,
-                                      max_features='auto', max_leaf_nodes=None, n_estimators=500, n_jobs=2,
-                                      oob_score=False, random_state=0, verbose=1, warm_start=False)
-    regressor.fit(x, y)
-
-    print(regressor.feature_importances_)
-
-    importances = regressor.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in regressor.estimators_],
-                 axis=0)
-
-    # Sorting features by importance
-    indices = np.argsort(importances)[::-1]
-
-    # indices = indecis[:40]
-
-    # Print the feature ranking
-    print("Feature ranking:")
-
-    for f in range(x.shape[1]):
-        print("%d. feature %d (%f)" %
-              (f + 1, indices[f], importances[indices[f]]))
-
-    # Plot the feature importances of the forest
-    plt.figure()
-    # plt.title("Feature importances")
-
-    plt.bar(range(x.shape[1]), importances[indices],
-            color="r", yerr=std[indices], align="center")
-    plt.yticks(rotation='vertical')
-    plt.xticks(range(x.shape[1]),
-               features[indices], rotation='vertical')
-    plt.xlim([-1, x.shape[1]])
-    plt.tight_layout()
-
-    plt.savefig('note.png')
-    if print_:
-        plt.show()
-
-    drop_features = dataset.columns[indices[num_features:]].values
-    return dataset.drop(drop_features, axis=1)
-
-
-def extract_data(datapath, normalize=True):
+def extract_data(datapath, normalize = True):
     datapath = datapath
     try:
         dataset = read_csv(datapath, index_col=0, sep=';')
@@ -87,45 +27,36 @@ def extract_data(datapath, normalize=True):
 
     dataset = dataset.dropna()
 
-    data_x = dataset.iloc[:, :-1]
-    data_y = dataset.iloc[:, -1:]
-    # Normalizing data (EXCEPT THE LABEL)
+    data_x = dataset.iloc[:,:-1]
+    data_y = dataset.iloc[:,-1:]
+    #Normalizing data (EXCEPT THE LABEL)
     if normalize:
 
-        x_scaler = MinMaxScaler(copy=True, feature_range=(0, 1))
+        x_scaler = MinMaxScaler(copy=True,feature_range=(0,1))
         data_x = x_scaler.fit_transform(data_x)
 
-        dataset.iloc[:, :-1] = data_x
-        dataset.iloc[:, -1:] = data_y
+        dataset.iloc[:,:-1] = data_x
+        dataset.iloc[:,-1:] = data_y
 
     return dataset
 
 
-def pearson(dataset):
 
-    pearson_corr = dataset.corr(method='pearson', min_periods=1)
+def extract_pearson_features(dataset, treshold = 0.5):
 
-    col_names = dataset.columns
-
-    ax = dataset.plot.scatter(x='YVIK-YtreVikna1..-V1-T0016A3 -0108', y='YVIK-YtreVikna1.-G1-T4015A3 -0104',
-                              c='rekkefølge', cmap='coolwarm', figsize=(20, 10))
-
-    plt.xlabel('Naselle vindhastighet(m/s) [Variabelkode:]')
-    plt.ylabel('Produksjon i Mega Watt (MW) [Variabelkode:]')
-
-    fig = ax.get_figure()
-    fig.savefig('Korrelasjon forecast error og .png')
-
+    pearson_corr = dataset.corr(method='pearson',min_periods = 1)
     topcorr = pearson_corr.iloc[-1]
 
     print('\nPearson correlation with total production as target\n')
     print(topcorr)
     print('\n')
 
+
+
     n_dropped = 0
 
-    for i, pear_val in enumerate(topcorr):
-
+    for i,pear_val in enumerate(topcorr):
+        
         if np.abs(pear_val) < treshold:
             print('dropping: ' + topcorr.keys()[i])
             dataset = dataset.drop(topcorr.keys()[i], axis=1)
@@ -136,39 +67,40 @@ def pearson(dataset):
     return dataset
 
 
-def extract_PCA_features(dataset, n_components=10):
+def extract_PCA_features(dataset, n_components = 10):
 
-    data_x = dataset.iloc[:, :-1]
-    data_y = dataset.iloc[:, -1:]
+
+    data_x = dataset.iloc[:,:-1]
+    data_y = dataset.iloc[:,-1:]
+
 
     pca = PCA(n_components=n_components)
     data_x_pca = pca.fit_transform(data_x)
 
-    data = np.concatenate((data_x_pca, data_y), axis=1)
+    data = np.concatenate((data_x_pca, data_y),axis = 1)
 
     return data, pca
+    
 
+def prediction(data, testsize = 0.15):
 
-def prediction(data, testsize=0.15):
-
-    x_train, x_test, y_train, y_test = train_test_split(
-        data[:, :-1], data[:, -1:], test_size=testsize, random_state=666)
+    x_train, x_test, y_train, y_test = train_test_split(data[:,:-1], data[:,-1:], test_size=testsize, random_state=666)
 
     ols = linear_model.LinearRegression()
-    model = ols.fit(x_train, y_train)
+    model = ols.fit(x_train,y_train)
 
     predictions = model.predict(x_test)
 
-    print('MAE: {}\n'.format(mean_absolute_error(y_test, predictions)))
+    print('MAE: {}\n'.format(mean_absolute_error(y_test,predictions)))
 
     return predictions, y_test
 
 
-def visualize(pred_list, legend, scope=0.2):
+def visualize(pred_list, legend, scope = 0.2):
 
     length = int(scope * pred_list[0].shape[0])
 
-    for i, pred in enumerate(pred_list):
+    for i,pred in enumerate(pred_list):
         plt.plot(pred[0:length])
 
     plt.legend(legend, loc='upper right')
@@ -201,19 +133,20 @@ def series_to_supervised(self, data, n_in=1, n_out=1, dropnan=True):
 
 
 if __name__ == '__main__':
-    datapath_old = os.path.join('..', 'data', 'Advanced_data2.csv')
-    datapath_simple = os.path.join('..', 'data', 'Data_simple.csv')
-    datapath_advanced = os.path.join(
-        '..', 'data', 'Bessaker Vindpark', 'data_bessaker_advanced.csv')
+    datapath_old = os.path.join('..','data', 'Advanced_data2.csv')
+    datapath_simple = os.path.join('..','data', 'Data_simple.csv')
+    datapath_advanced = os.path.join('..','data','Bessaker Vindpark', 'data_bessaker_advanced.csv')
+
 
     dataset = read_csv(datapath_advanced, index_col=0, sep=';')
+    
 
+    
     print(dataset.columns)
 
-    new_data = dataset[['BESS-Bessakerfj.-G2-T4015A3 -0104',
-                        'BESS-Bessakerfj.-G2-T4015A3 -0104']].head()
-    new_data.columns = ['production', 'target']
-
+    new_data = dataset[['BESS-Bessakerfj.-G2-T4015A3 -0104','BESS-Bessakerfj.-G2-T4015A3 -0104']].head()
+    new_data.columns = ['production','target']
+    
     print(new_data)
     # print(singlemill)
     # series_to_supervised()
@@ -222,10 +155,11 @@ if __name__ == '__main__':
     n_components = 12
     treshold = 0.7
 
+
     dataset = extract_data(datapath_old)
 
     # Extract PCA features
-    PCA_data, PCA_model = extract_PCA_features(dataset, n_components)
+    PCA_data, PCA_model = extract_PCA_features(dataset,n_components)
 
     # Only variables with acceptable pearson correlation with output is allowed to pass
     pearson_dataset = extract_pearson_features(dataset, treshold)
@@ -236,13 +170,11 @@ if __name__ == '__main__':
 
     print('Predicting with {} PCA features'.format(n_components))
     PCA_pred, _ = prediction(PCA_data, testsize)
-
-    print('Predicting with pearson correlations above {}, gives {} features'.format(
-        treshold, pearson_dataset.shape[1]))
+    
+    print('Predicting with pearson correlations above {}, gives {} features'.format(treshold, pearson_dataset.shape[1]))
     pearson_pred, y_test = prediction(pearson_dataset.values, testsize)
 
     # Compare the prediction methods
-    visualize([y_test, raw_pred, PCA_pred, pearson_pred],
-              legend=['GT', 'Raw', 'PCA', 'Pearson'], scope=0.2)
+    visualize([y_test,raw_pred,PCA_pred,pearson_pred],legend=['GT','Raw','PCA','Pearson'],scope = 0.2)
 
     exit(0)
